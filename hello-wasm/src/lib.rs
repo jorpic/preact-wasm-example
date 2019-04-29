4b6a351c7abf0457ba0b40360f5af6b9c4c335ef
Max Taldykin <jorpic@gmail.com>
2019-04-29 21:40:54 +0300

    Connect preact with wasm

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -8,12 +8,8 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, hello-wasm!");
+pub fn greet() -> String {
+    "Hello, wasm!".into()
 }
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import './style';
 import { Component } from 'preact';
 
+import('hello-wasm')
+  .then(wasm => console.log(wasm))
+  .catch(err => console.log(err));
+
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <h1>Hello, World!</h1>
+    );
+  }
 }
