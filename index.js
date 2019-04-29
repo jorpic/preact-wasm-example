5ece8f91e38161b0c9f1914b673f961cc7371739
Max Taldykin <jorpic@gmail.com>
2019-04-29 22:34:34 +0300

    Remove uninteresting code

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-import './style';
-import { Component } from 'preact';
-
 import('hello-wasm')
   .then(wasm => console.log(wasm))
   .catch(err => console.log(err));
 
-export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
-}
+const App = () => (<h1>Hello, World!</h1>);
+export default App;
